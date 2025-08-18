a3b509beeb83a57f0f726a49ecd966e412fc1145
soroush <slotfi909@gmail.com>
2025-08-18 20:18:24 +0330

    commented out neo-tree from plugins

--- nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
@@ -25,6 +25,7 @@ return {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'BurntSushi/ripgrep' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
