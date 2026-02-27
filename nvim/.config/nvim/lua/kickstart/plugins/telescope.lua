404ff732df16e9d894a862a7a3f3ca688d1013e2
soroush <slotfi909@gmail.com>
2026-02-27 15:55:05 +0330

    removed `ripgrep` from dependencies of telescope since it must be installed system-wide

--- nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/telescope.lua
@@ -25,7 +25,6 @@ return {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'BurntSushi/ripgrep' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
